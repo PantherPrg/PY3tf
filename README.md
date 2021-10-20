d06760e5369d502c1076e293fd2290b55f5c82e8
PantherPrg <74570815+PantherPrg@users.noreply.github.com>
2021-10-20 04:28:18 -0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,17 @@
+# PPSC_task_0: Title
+**| difficulty: 0/10 |**
+
+Background, instructions
+__________________________________________________________________________________
+**Input**:
+
+__________________________________________________________________________________
+**Output**:
+
+Make sure to add a newline at the end of your answer for it to match the answer format.
+__________________________________________________________________________________
+**optional challenge: write your code in under 0 lines**
+
+This problem deals with Pretty cool. **Do your research, google is your friend.**
+
+For additional help, either ask Paul, Joshua, Mr. Blatner, or any other staff member for assistance. We would be able to go over them during the club. **It can be an anonymous request if you would like.**
---
+++ main.py
@@ -0,0 +1,26 @@
+from workplace import workplace # <- - - - - - - Workplace Function
+
+# dont touch this stuff 
+
+taskNum = 0 # <- - - - - Task Number
+numtasks = 4 # <- - - - - Number of tasks, also edit files
+variables = {}
+perfect = True
+
+for i in range(numtasks):
+  # Argument formatting
+  testCase = open(("tC/tC" + str(i) + ".in"), "r").readlines()
+
+  print("-------------------<test-case-" + str(i) + ">-------------------")
+  variables["t{0}t".format(i)] = open(("tCA/tCA" + str(i) + ".out"), "r").read()
+
+  variables["t{0}a".format(i)] = workplace(testCase)
+
+  if variables["t{0}t".format(i)] == variables["t{0}a".format(i)]:
+    print("              ##< TESTCASE SOLVED >##\n\n\n")
+  else:
+    print("                    you failed\n\n\n")
+    perfect = False
+
+if perfect == True:
+  print("Congratulations! You successfully completed PY3_task_" + str(taskNum))
